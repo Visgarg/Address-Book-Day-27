3b77fea16889c07d0d1d555d1cde05b3e9413306
Vishal Garg <vishugarg10004@gmail.com>
2020-11-11 04:27:25 +0530

    [Vishal] added methods to get all contacts from database

--- AddressBook ADO.NET/Program.cs
+++ AddressBook ADO.NET/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook_ADO.NET
 {
@@ -6,7 +7,25 @@ namespace AddressBook_ADO.NET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Address Book Problem, ADO.NET use cases");
+            //instatiating address book operations
+            AddressBookOperations addressBookOperations = new AddressBookOperations();
+            //UC16 getting contact details
+            try
+            {
+                List<AddressBookContactDetails> contactDetailsList = addressBookOperations.GetAllContactDetails();
+                contactDetailsList.ForEach(contactDetails =>
+                {
+                    Console.WriteLine("ContactID:- " + contactDetails.contactID + " First Name:- " + contactDetails.firstName + " Last Name:- " + contactDetails.lastName + " Address:- " + contactDetails.address + " City:- " + contactDetails.city + " State:- " + contactDetails.state + " Zip:- " + contactDetails.zip + " phone number:- " + contactDetails.phoneNo + " Email:- " + contactDetails.eMail);
+                    Console.WriteLine("address Book Name id: -" + contactDetails.addressBookNameId + " address book name: -" + contactDetails.addressBookName);
+                    Console.WriteLine("Type id: -" + contactDetails.typeId+" type name: -" + contactDetails.typeName);
+                });
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
